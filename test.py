7e5374c0a371634b4521a718ea8b715a560aed0f
Charlie Linville <charlie@benlaura.com>
2025-08-11 10:48:39 -0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+from ibapi.contract import Contract
+import time
+class TradeApp(EWrapper, EClient): 
+    def __init__(self): 
+        EClient.__init__(self, self) 
+    def accountSummary(self, reqId: int, account: str, tag: str, value: str,currency: str):
+        print("AccountSummary. ReqId:", reqId, "Account:", account,"Tag: ", tag, "Value:", value, "Currency:", currency)
+    
+    def accountSummaryEnd(self, reqId: int):
+        print("AccountSummaryEnd. ReqId:", reqId)
+    
+app = TradeApp()      
+app.connect("127.0.0.1", 8000, clientId=0)
+time.sleep(1)
+app.reqAccountSummary(9001, "All", 'NetLiquidation')
+app.run()
