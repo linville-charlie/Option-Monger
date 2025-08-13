88888a3574fe55b87bdb560d901c2c6a940fb9b0
Charlie Linville <charlie@benlaura.com>
2025-08-13 12:08:31 -0600

    Fix notebook path in start_notebook.bat
    - Add cd .. to go to project root
    - Update path to notebooks/live_options_trading_optimized.ipynb

--- scripts/start_notebook.bat
+++ scripts/start_notebook.bat
@@ -2,5 +2,6 @@
 REM Quick start for Jupyter notebook with venv
 
 echo Starting Jupyter Notebook with OptionMonger venv...
+cd ..
 call venv\Scripts\activate.bat
-jupyter notebook live_options_trading_optimized.ipynb
+jupyter notebook notebooks\live_options_trading_optimized.ipynb
